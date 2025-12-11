65b1e56895c5704304d47f56e22bcbdaf6e6e6de
Cristopher <loyoladv@gmail.com>
2025-12-10 22:57:01 -0500

    Creacion de Entities

---
+++ src/Library.Domain/Entities/Books.cs
@@ -0,0 +1,15 @@
+namespace Library.Domain.Entities
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<Loans> Loans { get; set; } = new List<Loans>();
+    }
+}
+
---
+++ src/Library.Domain/Entities/Loans.cs
@@ -0,0 +1,14 @@
+namespace Library.Domain.Entities
+{
+    public class Loans
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public bool status { get; set; } = false;
+        public DateTime CreatedAt { get; set; }
+    } 
+}
+
